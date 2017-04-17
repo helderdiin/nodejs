1b09a13569d974a302d74e7c6ba9ab033f63080d
Helder Traci <helderdiin.work@gmail.com>
2017-04-17 12:07:30 -0300

    Escrevendo testes para as rotas

---
+++ test/routes.js
@@ -0,0 +1,54 @@
+import supertest from 'supertest';
+import test from 'tape';
+
+import app from '../app';
+
+const api = supertest(app);
+
+test('GET /health', t => {
+  api
+  .get('/health')
+  .expect('Content-type', /json/)
+  .expect(200)
+  .end((err, res) => {
+    if (err) {
+      t.fail(err);
+      t.end();
+    } else {
+      t.ok(res.body, 'It should have a response body');
+      t.equals(res.body.healthy, true, 'It should return a healthy parameter and it should be true');
+      t.end();
+    }
+  });
+});
+
+test('GET /docker', t => {
+  api
+  .get('/docker')
+  .expect('Content-type', /json/)
+  .expect(200)
+  .end((err, res) => {
+    if (err) {
+      t.fail(err);
+      t.end();
+    } else {
+      t.ok(res.body, 'It should have a response body');
+      t.equals(res.body.docker, 'rocks!', 'It should return a docker parameter with value rocks!');
+      t.end();
+    }
+  });
+});
+
+test('GET unknown route', t => {
+  api
+  .get(`/${Math.random() * 10}`)
+  .expect(404)
+  .end((err, res) => {
+    if (err) {
+      t.fail(err);
+      t.end();
+    } else {
+      t.end();
+    }
+  });
+});
